fa3605fc763a88b505c66340983c474bc7e483aa
User <sonael121@naver.com>
2024-06-30 14:54:27 +0900

    🎉 add : JwtUtil

---
+++ src/main/java/com/study/todoapp/jwt/JwtUtil.java
@@ -0,0 +1,113 @@
+package com.study.todoapp.jwt;
+
+import com.study.todoapp.enums.UserRoleEnum;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.security.Key;
+import java.security.Signature;
+import java.util.Base64;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+@Component
+@Slf4j
+public class JwtUtil {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String AUTHORIZATION_KEY = "authKey";
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    private long TOKEN_TIME = 60 * 60 * 1000L; // 1시간
+
+    // 로그 설정
+    public static final Logger logger = LoggerFactory.getLogger("JWT 관련 로그");
+
+    @Value("${JWT_SECRET_KEY}")
+    private String secret;
+
+    private Key key;
+
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secret);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String createToken(String username, UserRoleEnum role, String info) {
+
+        Date date = new Date();
+
+        return BEARER_PREFIX + Jwts.builder().setSubject(username)
+            .claim(AUTHORIZATION_KEY, role)
+            .claim("info", info)
+            .setExpiration(new Date(date.getTime() + TOKEN_TIME))
+            .setIssuedAt(date)
+            .signWith(key, signatureAlgorithm)
+            .compact();
+    }
+
+    // JWT 토큰 substring
+    public String substringToken(String tokenValue){
+        // 토큰인지 확인
+        if(StringUtils.hasText(tokenValue) && tokenValue.startsWith(BEARER_PREFIX)){
+            return tokenValue.substring(7);
+        }
+        logger.error("Not Found Token");
+        throw new NullPointerException("Not Found Token");
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException | SignatureException e) {
+            logger.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+        } catch (ExpiredJwtException e) {
+            logger.error("Expired JWT token, 만료된 JWT token 입니다.");
+        } catch (UnsupportedJwtException e) {
+            logger.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+        } catch (IllegalArgumentException e) {
+            logger.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+        }
+        return false;
+    }
+
+    // 헤더에 토큰 전달
+    public void addJwtToHeader(HttpServletResponse response, String headerName, String token){
+        response.setHeader(headerName, token);
+    }
+
+    // 토큰에서 사용자 정보 가져오기
+    public Claims getUserInfoFromToken(String token){
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    // HttpServletRequest 에서 토큰있는지 확인
+    public String getTokenFromRequest(HttpServletRequest request, String headerName){
+        String token = request.getHeader(headerName);
+        if(token != null && !token.isEmpty()){
+            return token;
+        }
+        return null;
+    }
+
+    // 사용자에게서 토큰 가져오기
+    public String getTokenFromRequest(HttpServletRequest req) {
+        return req.getHeader(AUTHORIZATION_HEADER);
+    }
+}
